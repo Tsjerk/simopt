204cfe5599134f9d0b091b9da6b9d48decbd873b
Tsjerk A. Wassenaar <tsjerkw@gmail.com>
2017-03-13 16:24:41 +0100

    Added initial code and pip setup

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+setup(
+    name='simopt',
+    version='0.1',
+    description='SIMple OPTion parser',
+    long_description=open("README.md").read(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='simple option parser',
+    url='http://github.com/Tsjerk/simopt',
+    author='Tsjerk A. Wassenaar',
+    author_email='tsjerkw@gmail.com',
+    license='MIT',
+    packages=['simopt'],
+    install_requires=[
+    ],
+    #      test_suite='nose.collector',
+    #      tests_require=['nose', 'nose-cover3'],
+    #      entry_points={
+    #      },
+    include_package_data=True,
+    zip_safe=False
+)
+
+
+
